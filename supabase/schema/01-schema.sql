2277ba840a93e2fed7aa30ef61cabc6d6daf1d95
Joche3023 <j.zumbado.1@estudiantec.cr>
2026-07-17 23:55:38 -0600

    Run CI

--- supabase/schema/01-schema.sql
+++ supabase/schema/01-schema.sql
@@ -3,7 +3,7 @@
 -- =================================================
 
 -----------------------------------------------
--- ENUMs
+-- ENUMS
 -----------------------------------------------
 CREATE TYPE election_status AS ENUM ('DRAFT', 'SCHEDULED', 'OPEN', 'CLOSED', 'SCRUTINIZED', 'ARCHIVED');
 CREATE TYPE auth_method_type AS ENUM ('MICROSOFT');
